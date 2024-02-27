519b28ea9165b67b94866cc3dc6ec8d043720c0c
jarethholt <jareth.holt@gmail.com>
2024-02-27 21:00:50 +0100

    feat: Added necessary math functions
    Added the MyMath namespace and class, providing functions appropriate for testing whether doubles are close enough to be equal.

---
+++ GreatCircle/MyMath/MyMath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatCircle.MyMath;
+
+internal class MyMath
+{
+    public static bool IsCloseTo(
+        double value,
+        double target,
+        double relativeTolerance = MyMathHelper.relativeTolerance,
+        double absoluteTolerance = MyMathHelper.absoluteTolerance)
+    {
+        /* Determine whether a given value is close to the target.
+         * 
+         * Based on numpy.isclose, comparing the absolute error to a combined tolerance.
+         */
+        return Math.Abs(value - target) <= absoluteTolerance + relativeTolerance * Math.Abs(target);
+    }
+
+    public static bool AreClose(double value1, double value2, double relativeTolerance = MyMathHelper.relativeTolerance, double absoluteTolerance = MyMathHelper.absoluteTolerance)
+    {
+        /* Determine whether two values are close to each other.
+         * 
+         * Based on numpy.isclose but treats value1 and value2 symmetrically.
+         */
+        double scale = 0.5 * (Math.Abs(value1) + Math.Abs(value2));
+        return Math.Abs(value1 - value2) <= absoluteTolerance + relativeTolerance * scale;
+    }
+
+}
---
+++ GreatCircle/MyMath/MyMathHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatCircle.MyMath;
+
+internal static class MyMathHelper
+{
+    internal const double relativeTolerance = 1e-6;
+    internal const double absoluteTolerance = 1e-6;
+}
