ff5771423dd0ae8614020c45cd1418db046a30e4
jarethholt <jareth.holt@gmail.com>
2024-02-28 13:41:45 +0100

    tests: Add tests for Coordinates
    Add tests for the Coordinates class. Includes:
    - Test that out-of-range latitude throws exception
    - `Longitude` is reset to the correct range
    - `Longitude` of a polar point is reset to zero
    - `Longitude` of a dateline point is at -180
    - `IsAPole` correctly marks polar and non-polar points
    - `IsAntipodalTo` marks polar and non-polar points

---
+++ GreatCircle.Tests/CoordinatesTests.cs
@@ -0,0 +1,58 @@
+﻿namespace GreatCircle.Tests;
+
+public class CoordinatesTests
+{
+    [Fact]
+    public void Coordinates_Latitude_OutOfRange()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => new Coordinates.Coordinates(100, 0));
+    }
+
+    [Fact]
+    public void Coordinates_Longitude_ResetToRange()
+    {
+        double longitude = 50;
+        Assert.Equal(longitude, new Coordinates.Coordinates(0, longitude+360).Longitude);
+    }
+
+    [Fact]
+    public void Coordinates_PolarPoint_ResetsLongitude()
+    {
+        Assert.Equal(0, new Coordinates.Coordinates(90, 30).Longitude);
+    }
+
+    [Fact]
+    public void Coordinates_DateLineAtNeg180()
+    {
+        Assert.Equal(-180, new Coordinates.Coordinates(0, 180).Longitude);
+    }
+
+    [Fact]
+    public void IsAPole_PolarPoint()
+    {
+        Assert.True(Coordinates.Coordinates.NorthPole().IsAPole());
+    }
+
+    [Fact]
+    public void IsAPole_NonPolarPoint()
+    {
+        Assert.False(Coordinates.Coordinates.Origin().IsAPole());
+    }
+
+    [Fact]
+    public void IsAntipodalTo_PolarPoint()
+    {
+        Assert.True(
+            Coordinates.Coordinates.NorthPole().IsAntipodalTo(
+            Coordinates.Coordinates.SouthPole()));
+    }
+
+    [Fact]
+    public void IsAntipodalTo_NonPolarPoint()
+    {
+        Assert.True(
+            Coordinates.Coordinates.Origin().IsAntipodalTo(
+            new Coordinates.Coordinates(0, 180)));
+    }
+}
