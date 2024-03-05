b38c4410953c8b45602c14cdb98c995cd0199e91
jarethholt <jareth.holt@gmail.com>
2024-03-05 11:33:39 +0100

    doc: Provide comments at the top of each class file

--- GreatCircle/Coordinates/Coordinates.cs
+++ GreatCircle/Coordinates/Coordinates.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Class describing a coordinate (latitude/longitude) on the sphere.
+
 using GreatCircle.MyMathUtils;
 
 namespace GreatCircle.Coordinates;
--- GreatCircle/GreatCircle.cs
+++ GreatCircle/GreatCircle.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Class providing the functionality of a great circle path.
 
 namespace GreatCircle;
 
