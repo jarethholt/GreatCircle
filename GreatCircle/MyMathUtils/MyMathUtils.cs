671d7df458d21a8959bff1753b12bc892cd78e20
jarethholt <jareth.holt@gmail.com>
2024-03-05 11:31:25 +0100

    refactor: Change structure of MyMathUtils
    - Change `MyMathHelper` into a public struct `MyMathDefaults` and move to the `MyMathUtils` file
    - Change `MyMath` into a static class

--- GreatCircle/MyMathUtils/MyMathUtils.cs
+++ GreatCircle/MyMathUtils/MyMathUtils.cs
@@ -1,30 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Utility for checking whether doubles are close within a tolerance
 
 namespace GreatCircle.MyMathUtils;
 
-public class MyMath
+public struct MyMathDefaults
+{
+    public const double relativeTolerance = 1e-6;
+    public const double absoluteTolerance = 1e-6;
+}
+
+public static class MyMath
 {
     public static bool IsCloseTo(
         double value,
         double target,
-        double relativeTolerance = MyMathHelper.relativeTolerance,
-        double absoluteTolerance = MyMathHelper.absoluteTolerance)
+        double relativeTolerance = MyMathDefaults.relativeTolerance,
+        double absoluteTolerance = MyMathDefaults.absoluteTolerance)
     {
         /* Determine whether a given value is close to the target.
-         * 
          * Based on numpy.isclose, comparing the absolute error to a combined tolerance.
          */
         return Math.Abs(value - target) <= absoluteTolerance + relativeTolerance * Math.Abs(target);
     }
 
-    public static bool AreClose(double value1, double value2, double relativeTolerance = MyMathHelper.relativeTolerance, double absoluteTolerance = MyMathHelper.absoluteTolerance)
+    public static bool AreClose(
+        double value1,
+        double value2,
+        double relativeTolerance = MyMathDefaults.relativeTolerance,
+        double absoluteTolerance = MyMathDefaults.absoluteTolerance)
     {
         /* Determine whether two values are close to each other.
-         * 
          * Based on numpy.isclose but treats value1 and value2 symmetrically.
          */
         double scale = 0.5 * (Math.Abs(value1) + Math.Abs(value2));
