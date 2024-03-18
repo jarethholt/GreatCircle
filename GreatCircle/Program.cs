bdad7c004c6387cc641c9fe7c00275d26f70f94f
jarethholt <jareth.holt@gmail.com>
2024-03-18 15:23:14 +0100

    feat: Expand example calculation

--- GreatCircle/Program.cs
+++ GreatCircle/Program.cs
@@ -13,10 +13,22 @@ internal class Program
             = GreatCirclePath.PathAndAngleBetweenPoints(valparaiso, shanghai);
 
         Console.WriteLine(
-            $"Central angle between these points: {angle:F2}{Angles.Degree}"
+            $"Central angle between these points (should be 168.56{Angles.Degree}): "
+            + $"{angle:F2}{Angles.Degree}"
         );
         Console.WriteLine(
-            $"Required initial azimuth: {path.InitialAzimuth:F2}{Angles.Degree}"
+            $"Required initial azimuth (should be -94.41{Angles.Degree}): "
+            + $"{path.InitialAzimuth:F2}{Angles.Degree}"
         );
+
+        (Coordinate target, double finalAzimuth) = path.DisplaceByAngle(angle);
+        Console.WriteLine(
+            $"Displacing by {angle:F2}{Angles.Degree} from {valparaiso.ToString("F1")} "
+            + $"along heading {path.InitialAzimuth:F2}{Angles.Degree} gets us to "
+            + $"{target.ToString("F1")} (should be {shanghai.ToString("F1")})."
+        );
+        Console.WriteLine(
+            $"We also get a final azimuth (should be -78.42{Angles.Degree}): "
+            + $"{finalAzimuth:F2}");
     }
 }
